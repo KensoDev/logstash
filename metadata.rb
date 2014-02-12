87de9ff12073baf8a0b891442241c90de952395e
Justin Steffy <steffy@orgsync.com>
2014-02-12 16:22:53 -0600

    add metadata file

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'logstash'
+maintainer       'OrgSync, Inc.'
+maintainer_email 'ops@orgsync.com'
+license          'All rights reserved'
+description      'Installs/Configures logstash'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+supports 'debian'
