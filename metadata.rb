4b32f53960b9650ae5ceb950aae5add979bb8040
Justin Steffy <steffy@orgsync.com>
2014-02-13 11:43:46 -0600

    update the metadata file to reflect recipes and java

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,11 @@ maintainer_email 'ops@orgsync.com'
 license          'All rights reserved'
 description      'Installs/Configures logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.0'
+
+recipe 'logstash::server', 'Install and configure a logstash server'
+recipe 'logstash::agent', 'Install and configure a logstash agent'
+
+recommends 'java', '~> 1.19'
 
 supports 'debian'
