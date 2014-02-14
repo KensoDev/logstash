e3ebe7a8f0394a78a57b0888aeecfdc33d9037e4
Justin Steffy <steffy@orgsync.com>
2014-02-14 11:35:28 -0600

    refactor specs

--- test/integration/server/serverspec/localhost/server_spec.rb
+++ test/integration/server/serverspec/localhost/server_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
 describe 'logstash::server' do
-  describe group('logstash') do
-    it { should exist }
+
+  it 'ensures the logstash group' do
+    expect(group('logstash')).to exist
   end
 
-  describe user('logstash') do
-    it { should exist }
-    it { should belong_to_group('logstash') }
-    it { should have_login_shell('/bin/false') }
+  context 'ensures the logstash user' do
+    let(:logstash_user) { user('logstash') }
+
+    it 'exists' do
+      expect(logstash_user).to exist
+    end
+
+    it 'is in the logstash group' do
+      expect(logstash_user).to belong_to_group('logstash')
+    end
+
+    it 'has no shell' do
+      expect(logstash_user).to have_login_shell('/bin/false')
+    end
   end
 end
