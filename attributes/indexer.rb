19885593e5eb5590a6c5686f3b02a409ac2c028d
Justin Steffy <steffy@orgsync.com>
2014-02-27 14:18:15 -0600

    Refactor attributes for default and indexer.
    - Remove some unused attributes
    + Wrap some long lines

--- attributes/indexer.rb
+++ attributes/indexer.rb
@@ -1,7 +1,9 @@
 # Encoding: utf-8
-default['logstash']['indexer']['log_file'] = ::File.join(node['logstash']['logdir'], 'indexer.log')
+default['logstash']['indexer']['log_file'] =
+  ::File.join(node['logstash']['logdir'], 'indexer.log')
 
-default['logstash']['indexer']['config_file'] = ::File.join(node['logstash']['confdir'], 'indexer.conf')
+default['logstash']['indexer']['config_file'] =
+  ::File.join(node['logstash']['confdir'], 'indexer.conf')
 
 default['logstash']['indexer']['config_data'] = {
   input: {
@@ -21,5 +23,8 @@ default['logstash']['indexer']['config_data'] = {
 
 default['logstash']['indexer']['daemon_name'] = 'logstash-indexer'
 default['logstash']['indexer']['jvm_opts'] = '-server -Xms1024M -Xmx1024M'
-default['logstash']['indexer']['es_path_home'] = node['logstash']['basedir']
 default['logstash']['indexer']['sincedb_dir'] = node['logstash']['logdir']
+
+# the default config uses embedded es, so we need to put its home path somewhere
+# that the logstash user can write...
+default['logstash']['indexer']['es_path_home'] = node['logstash']['basedir']
