8386e796349f4ec3a86eb4e6f4df3593103ebd58
Justin Steffy <steffy@orgsync.com>
2014-02-27 16:44:53 -0600

    Time for a version bump

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,9 @@ maintainer_email 'ops@orgsync.com'
 license          'All rights reserved'
 description      'Installs/Configures logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
+recipe 'logstash::default', 'Install and configure logstash'
 recipe 'logstash::indexer', 'Install and configure a logstash indexer'
 recipe 'logstash::agent', 'Install and configure a logstash agent'
 
