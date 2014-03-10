bc7e960d15f753cbaf29112fdb4a0cbb7de79c29
Justin Steffy <steffy@orgsync.com>
2014-03-10 15:22:42 -0500

    Fix init script `stop` from killing all java.
    + Fix test to use ps and grep to determine if process is running
      - Really should fix service reporting the wrong thing, but thats
        another issue.
    + Remove the default "reap child processes" kill from init script

--- test/integration/both/serverspec/localhost/both_spec.rb
+++ test/integration/both/serverspec/localhost/both_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 describe 'both agent and indexer' do
   include_context 'common'
 
+  # service is lying to us, and using `process` doesn't work right
+  def ps_grep(install_type)
+    backend.run_command("ps -ef | grep #{install_type}.conf | grep -v grep")
+  end
+
   it 'has indexer running' do
-    expect(service('logstash-indexer')).to be_running
+    expect(ps_grep('indexer')).to be_success
   end
 
   it 'has agent running' do
-    expect(service('logstash-agent')).to be_running
+    expect(ps_grep('agent')).to be_success
   end
 end
